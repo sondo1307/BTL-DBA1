bb2c3cb493829dcc63ad2f2db14b1053b2f70d61
SonDo <kuropentakill@gmail.com>
2025-08-22 10:03:26 +0700

    1. Add csv convert to datagrid clomun TOOL 2. Doing TranDauDetail

---
+++ Assets/Script/TempSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Script
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class TempSO : ScriptableObject
+    {
+        
+    }
+}
